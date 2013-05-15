f8a6edef36510ff561686445c50a5ede5fe42f8c
Xycl <xycl2008@gmail.com>
2013-05-15 15:01:07 +0200

    cleanup

--- default.py
+++ default.py
@@ -1,161 +1,212 @@
-# Open Source Initiative OSI - The MIT License (MIT):Licensing
-#[OSI Approved License]
-#The MIT License (MIT)
-
-#Copyright (c) 2011 N.K.
-
-#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-
-# ################################################################################
-# author: Xycl, nk
-# ################################################################################
-
-
-__addonname__    = "script.service.fritzbox"
+from pprint import pformat
 
 import xbmc, xbmcaddon
 import socket
 import os
-import errno
-from types import *
-from time import sleep
-import threading
+import re
+import datetime
+import time
 
-# own imports
-import resources.lib.fritzAddressbook as fritzAddressbook
-import resources.lib.common as common
-from resources.lib.service import IncomingCallThread
+from lib.PytzBox import PytzBox
 
-# Addressbook for lookup
-xbmctelefonbuch = {}
 
+# Script constants
+__addon__       = xbmcaddon.Addon()
+__addon_id__    = "script.service.fritzbox"
+#__version__     = "1"
 
 
 
+class FritzCallmonitor():
 
-DEFAULT_IMG = xbmc.translatePath(os.path.join( "special://home/", "addons", __addonname__, "media","default.png"))
+    __pytzbox = None
+    __fb_phonebook = None
 
-# Get settings
-ip = common.getaddon_setting( "S_IP" ) # return FritzIP setting value 
-dur = common.getaddon_setting( "S_DURATION" ) # return Anzeigedauer
-durdict = {'0': '1000','1': '2000' ,'2':'3000','3':'4000','4':'5000','5':'8000','6':'10000','7':'15000','8':'0'}
-duration = durdict.get(dur, 5000) # Unit conversion Seconds_2_Milliseconds, NotificationDialog wants Milliseconds
+    def error(*args, **kwargs):
+        xbmc.log("ERROR: %s %s" % (args, kwargs))
 
-useFritzAB = common.getaddon_setting( "AB_Fritzadress" )
-fritzAddressURL = common.getaddon_setting( "AB_Adressbookpath")
-PicFolder = common.getaddon_setting( "AB_Pics" )
-#debuggen = __settings__.getSetting("S_DEBUG")
+    class CallMonitorLine(dict):
 
-parameterstring = "Fritzbox: Ip Adresse definiert als '%s'" % ( ip)
-common.log('',parameterstring)
+        command = None
 
+        def __init__(self, response, **kwargs):
 
-def errorMsg(aList):
-    xbmc.log("Unhandled State")
+            self.__responses = dict()
+            if isinstance(response, str) or isinstance(response, unicode):
+                response = response.split(';')
+            self.command = response[1]
 
+            if self.command == 'CALL':
+                self['date'] = response[0]
+                self['connection_id'] = response[2]
+                self['extension'] = response[3]
+                self['number_used'] = response[4]
+                self['number_called'] = response[5]
+                self['sip'] = response[6]
 
-def handleOutgoingCall(aList):
-    #datum;CALL;ConnectionID;Nebenstelle;GenutzteNummer;AngerufeneNummer;
-    #[192.168.178.1] 03.01.12 22:09:56;CALL;0;0;123456;017500000;SIP1;
-    datum, funktion, connectionID, Nebenstelle, GenutzteNummer, AngerufeneNummer, sip,  leer = aList
-    logtext = ('Ausgehender Anruf an %s von Nr: %s, am %s' % (AngerufeneNummer, GenutzteNummer, datum))
-    heading = "Ausgehender Anruf"
-    text = "Angerufene Nr. %s von Apparat Nr: %s" % (AngerufeneNummer, GenutzteNummer)
-    common.log('',logtext)
-    #xbmc.executebuiltin("Notification("+heading+","+text+","+duration+","+DEFAULT_IMG+")")
-    common.show_notification(heading, text, duration, DEFAULT_IMG)
+            elif self.command == 'RING':
+                self['date'] = response[0]
+                self['connection_id'] = response[2]
+                self['number_caller'] = response[3]
+                self['number_called'] = response[4]
+                self['sip'] = response[5]
 
+            elif self.command == 'CONNECT':
+                self['date'] = response[0]
+                self['connection_id'] = response[2]
+                self['extension'] = response[3]
+                self['number'] = response[4]
 
-def handleIncomingCall(aList):
-    #datum;RING;ConnectionID;Anrufer-Nr;Angerufene-Nummer;sip;
-    #[192.168.178.1] 03.01.12 21:52:21;RING;0;017100000;012345;SIP2;
-    datum, funktion, connectionID, anruferNR, angerufeneNR, sip, leer = aList
-    logtext = ('Eingehender Anruf von %s auf Apparat %s' % (aList[3], aList[4]))
-    heading = 'Eingehender Anruf'
-    anrufer = xbmctelefonbuch.get(aList[3],'Unbenannt')
-    PIC = xbmc.translatePath(os.path.join(PicFolder,aList[3]+".png"))
-    text = 'von %s [%s]' % (anrufer, aList[3])
-    common.log('',"FRIIIITZ: " + PIC)
-    common.log('',logtext)
-    try:
-        open(PIC)
-    except:
-        PIC = DEFAULT_IMG
-     
-    #xbmc.executebuiltin("Notification("+heading+","+text+","+duration+","+PIC+")")
-    common.show_notification(heading, text, duration, PIC)
-    
-
-def handleConnected(aList):
-    #datum;CONNECT;ConnectionID;Nebenstelle;Nummer;
-    datum, funktion, connectionID, nebenstelle, nummer, leer = aList
-    logtext = ('Verbunden mit %s' % (nummer))
-    #print text
-    heading = 'Verbindung hergestellt'
-    text = 'mit %s' % (nummer)
-    common.log('',logtext)
-    #xbmc.executebuiltin("Notification("+heading+","+text+","+duration+","+DEFAULT_IMG+")")
-    common.show_notification(heading, text, duration, DEFAULT_IMG)
-    
-
-def handleDisconnected(aList):
-    #datum;DISCONNECT;ConnectionID;dauerInSekunden;
-    #[192.168.178.1] 03.01.12 22:12:56;DISCONNECT;0;0;
-    datum, funktion, connectionID, dauer,  leer = aList
-    text = ('Anrufdauer: %i Minuten' % (int(int(dauer)/60)))
-    heading = "Verbindung beendet"
-    #print text
-    common.log('',text)
-    #xbmc.executebuiltin("Notification(XBMC-Fritzbox,"+text+","+duration+","+DEFAULT_IMG+")")
-    common.show_notification(heading, text, duration, DEFAULT_IMG)
-
-fncDict = {'CALL': handleOutgoingCall, 'RING': handleIncomingCall, 'CONNECT': handleConnected, 'DISCONNECT': handleDisconnected}
+            elif self.command == 'DISCONNECT':
+                self['date'] = response[0]
+                self['connectionID'] = response[2]
+                self['duration'] = response[3]
 
 
-if useFritzAB: 
-    tmp=fritzAddressbook.Fritzboxtelefonbuch(xbmctelefonbuch,fritzAddressURL)
-    xbmctelefonbuch = tmp.getTelefonbuch()
-    
-common.log('',"ABFritzbox: "+ str(useFritzAB))
+            if 'date' in self:
+                #noinspection PyBroadException
+                try:
+                    self['date'] = datetime.datetime.strptime(self['date'].strip(), '%d.%m.%y %H:%M:%S')
+                except Exception:
+                    pass
+
+            if 'duration' in self:
+                #noinspection PyBroadException
+                try:
+                    self['duration'] = datetime.timedelta(seconds=int(self['duration']))
+                except Exception:
+                    pass
 
 
-try:
-    common.log(__addonname__,'started', xbmc.LOGNOTICE)    
-    response = ''
-    worker_thread = IncomingCallThread(ip)
-    common.log(__addonname__,'IncomingCallThread', xbmc.LOGNOTICE)    
-    worker_thread.setDaemon(True)
-    common.log(__addonname__,'setDaemon', xbmc.LOGNOTICE)    
-    worker_thread.start()
-    common.log(__addonname__,'start', xbmc.LOGNOTICE)    
-    
-    while (not xbmc.abortRequested):
+        def __getattr__(self, item):
+            if item in self:
+                return self[item]
+            else:
+                return False
+
+
+    def equalNumbers(self, a, b):
+        a = str(a).strip()
+        b = str(b).strip()
+
+        a = str(re.sub('[^0-9]*', '', a))
+        b = str(re.sub('[^0-9]*', '', b))
+
+        if a.startswith('00'): a = a[4:]
+        a = a.lstrip('0')
+
+        if b.startswith('00'): b = b[4:]
+        b = b.lstrip('0')
+
+        a = a[-len(b):]
+        b = b[-len(a):]
+
+        return (a == b)
+
+
+    def getNameByNumber(self, request_number):
+
+        if __addon__.getSetting( "AB_Fritzadress" ) == 'true':
+
+            if self.__pytzbox is None:
+
+                password = False
+                if __addon__.getSetting( "AB_FritzboxPassword" ) and len(str(__addon__.getSetting( "AB_FritzboxPassword" ))) > 0:
+                    password = __addon__.getSetting( "AB_FritzboxPassword" )
+
+                self.__pytzbox = PytzBox.PytzBox(password=password, host=__addon__.getSetting( "S_IP" ))
+
+                if password:
+                    self.__pytzbox.login()
+
+            if self.__fb_phonebook is None:
+                self.__fb_phonebook = self.__pytzbox.getPhonebook()
+
+            if isinstance(self.__fb_phonebook, dict):
+                for entry in self.__fb_phonebook:
+                    if 'numbers' in self.__fb_phonebook[entry]:
+                        for number in self.__fb_phonebook[entry]['numbers']:
+                            if self.equalNumbers(number, request_number):
+                                return entry
+
+        return False
+
+
+
+    def handleOutgoingCall(self, line):
+        name = self.getNameByNumber(line.number_called) or 'Unbekannt'
+        self.Notification("Ausgehender Anruf", "zu %s [%s] (von %s)" % (name, line.number_called, line.number_used))
+
+    def handleIncomingCall(self, line):
+        name = self.getNameByNumber(line.number_caller) or 'Unbekannt'
+        print xbmc.Player().isPlaying()
+        if xbmc.Player().isPlaying():
+            xbmc.Player().pause()
+        self.Notification('Eingehender Anruf', 'Von %s [%s]' % (name, line.number_caller))
+
+    def handleConnected(self, line):
+        if __addon__.getSetting( "AC_Pause" )  == 'true':
+            xbmc.Player().pause()
+        name = self.getNameByNumber(line.number) or 'Unbekannt'
+        self.Notification('Verbindung hergestellt', 'Mit %s [%s]' % (name, line.number))
+
+    def handleDisconnected(self, line):
+        self.Notification('Verbindung beendet', 'Dauer: %sh' % str(line.duration))
+
+    def Notification(self, title, text, duration=False, img=False):
+        xbmc.log("%s: %s" % (title, text))
+        xbmc.executebuiltin('PingApp')
+        if not duration:
+            duration = __addon__.getSetting( "S_DURATION" )
+            duration = int(duration)*1000
+        if not img:
+            img = xbmc.translatePath(os.path.join( xbmcaddon.Addon().getAddonInfo('path'), "media","default.png"))
+        return xbmc.executebuiltin('Notification("%s", "%s", %d, "%s")' % (title, str(text), duration, img))
+
+    def start(self):
+        ip = __addon__.getSetting( "S_IP" )
         try:
-            sleep(0.5)
-            common.log(__addonname__,'Loop', xbmc.LOGNOTICE)    
-            
-            IncomingCallThread.response_lock.acquire()
-            response = IncomingCallThread.response
-            common.log(__addonname__,'Response ' +  response, xbmc.LOGNOTICE)    
-            IncomingCallThread.response = ''
-            IncomingCallThread.response_lock.release()            
-            if response != '':
-                log= "[%s] %s" % (ip,response)
-                common.log('', log)
-                items = response.split(';')
-                fncDict.get(items[1], errorMsg)(items)
-                response = ''
-        except IndexError:
-            text = 'ERROR: Something is wrong with the message from the fritzbox. unexpected firmware maybe'
-            common.log(__addonname__,text)
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip, 1012))
+        except Exception, e:
+            self.Notification('Fritzbox nicht erreichbar', 'Konnte keine Verbindung zur Fritzbox herstellen (%s)' % e)
+        else:
+            xbmc.log('connected to fritzbox callmonitor')
+            s.settimeout(0.2)
 
-        except Exception, msg:
-            common.log(__addonname__,msg)
-    common.log(__addonname__,'XBMC sent an abort request')
-    
-except Exception, msg:
-    common.log(__addonname__,msg)
+            while not xbmc.abortRequested:
+
+                try:
+
+                    message = s.recv(1024)
+                    line = self.CallMonitorLine(message)
+                    xbmc.log(str(line))
+
+                    {
+                        'CALL': self.handleOutgoingCall,
+                        'RING': self.handleIncomingCall,
+                        'CONNECT': self.handleConnected,
+                        'DISCONNECT': self.handleDisconnected
+                    }.get(line.command, self.error)(line)
+
+                except IndexError:
+                    xbmc.log('ERROR: Something is wrong with the message from the fritzbox. unexpected firmware maybe')
+
+                except socket.timeout:
+                    pass
+
+                except socket.error, e:
+                    xbmc.log('ERROR: Could not connect %s on port 1012. Have you activated the Callmonitor via #96*5*' % ip)
+                    xbmc.log(pformat(e))
+
+                except Exception, e:
+                    xbmc.log(pformat(e))
+
+            s.close()
+            xbmc.log("XBMC-Fritzbox Addon beendet.")
+
+
+if __addon__.getSetting( "S_STARTUPSLEEP" ):
+    time.sleep(int(__addon__.getSetting( "S_STARTUPSLEEP" )))
+
+FritzCallmonitor().start()
